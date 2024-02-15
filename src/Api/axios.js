8eb1cd5e9ebd6a2f86ddf0dea0f283d0c775f9b5
lemegudata@gmail.com <146845168+Lamibane@users.noreply.github.com>
2024-02-15 11:54:29 +0300

    api

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://amazonbackendclone.onrender.com",
+  baseURL: "https://amazon-backendapi.onrender.com",
 });
 
 export { axiosInstance };
